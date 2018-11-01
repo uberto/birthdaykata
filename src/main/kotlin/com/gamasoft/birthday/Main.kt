152a25756530fe970b82a1b623e14fc8a2b0543c
Uberto Barbini <uberto.barbino@springernature.com>
2018-11-01 22:59:56 +0000

    code cleanup

--- src/main/kotlin/com/gamasoft/birthday/Main.kt
+++ src/main/kotlin/com/gamasoft/birthday/Main.kt
@@ -3,16 +3,17 @@ package com.gamasoft.birthday
 import java.time.LocalDate
 
 fun main(args: Array<String>){
+
+    val filename = args[1]
     val emplFilter = EmployeeBirthdayFilter(LocalDate.now())
-
-    val reader = FileReader(args[1]){CsvRow(it)}
-
-    val employees = reader
-            .map { it.toEmployee() }
-            .runReader { it.filter(emplFilter).toList() }
-
     val emailTemplate = EmailTemplate("Happy birthday, dear %!")
 
-    val emails = employees.map(emailTemplate)
+    val emails = FileReader(filename){CsvRow(it)}
+            .map { it.toEmployee() }
+            .runReader { it.filter(emplFilter).toList() }
+            .map(emailTemplate)
+            .forEach { println("Sending email: $it") }
+
+
 
 }
